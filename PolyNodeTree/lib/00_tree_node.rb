b8b77bef555f01aa37cd29485a86399fa7c61e45
Hammad Khalid <hammaddotkhalid@gmail.com>
2021-01-15 14:08:23 -0500

    Created PolyNodeTree#initialize and getters for ivars

--- PolyNodeTree/lib/00_tree_node.rb
+++ PolyNodeTree/lib/00_tree_node.rb
@@ -1,3 +1,16 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :node
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=()
+        self.parent = 
+    end
+
+
 end

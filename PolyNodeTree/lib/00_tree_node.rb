0e36471fb2e6a407be7f1a187248ef686243a784
tahj <tahjharris00@gmail.com>
2021-01-15 15:03:05 -0600

    Debugging our #parents=

--- PolyNodeTree/lib/00_tree_node.rb
+++ PolyNodeTree/lib/00_tree_node.rb
@@ -1,21 +1,26 @@
 class PolyTreeNode
 
-    attr_reader :parent, :children, :value
-
+    attr_reader :parent, :children, :value 
+    attr_writer :parent, :children, :value
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
     end
 
-    def parent=(node)
-        # self.parent ||= node
-        # self.parent = node
+    # def parent=(new_parent)
+    #     return self.parent if new_parent == nil
+    #         if self.parent != nil
+    #             self.parent.children.reject! {|child| child == self}
+    #              self.parent = new_parent
+    #         # else
+    #         #     self.parent = new_parent
+    #         end
 
-        if !node.children.any?{|child| child == self}
-            node.children.unshift(self)
-        end
-    end
+    #     if !new_parent.children.any?{|child| child == self}
+    #         new_parent.children.unshift(self)
+    #     end
+    # end
 
     def add_child
         
